6537ada596ba34ff4fa767c845747d2bda89046b
tmishra17 <jetmi@live.com>
2023-11-11 22:17:19 -0800

    Merged conflicts

--- pyfiles/transport.py
+++ pyfiles/transport.py
@@ -32,11 +32,7 @@ def findRouteAPI(start_coordinates, dest_coordinates, route_type):
     payload = {}
 
     headers = {
-<<<<<<< Updated upstream
-        'Authorization': f'Bearer {apikeys.TOKEN}'
-=======
         'Authorization': f'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6Imxlamd5NzNhMTQiLCJ0b2tlbiI6eyJpdiI6IjVhZTA0OGZlYTVkMjU3ZmQ0MTdjMTIyYjNiZGU0MmRkIiwiY29udGVudCI6IjNmZTRlYTYzZjE3NjdjYTgwZjVkNzVlZjUwNTJmZWU0YTVkOGQ5Y2E3YjAyMDY3MGEwZGUyY2JkMWRkMjYzNTUwMjMwNzgwNWYwOTVjMzg5YzFiNTExYzlkNmUzODQzZWE1YjFkMTFmOGM1OWUyMjJjYTZmYzVlYjA3N2JlODYxYzdhNWY2ZGY2NDMzYzM4OTA1ZWYwNTE4MGQxOWY2OTU2NTk5MmMxZjZlZTMxNTJiZGUxMWVkM2U2OWFmOGRmMzhkMzU0OTIzNjE3MDlhYWVlYzFlOThiNzY2MWUzM2FiOGI2MzhiNDZmM2RmYjgzMTExNGY4MThmYmU5YWUwYmQwMmE4MDMzYTk0MTI3NWM2OTg4YzQ0MzM2ZTIwZTFmMmFkYzMzZDMyYmUyMzZkZTA0ZTk5Y2E0ZWJhZDY3NDVjMDBhMjIzYjgxZWRmN2NmODdlMmQzOTk1ZmM3YjNlNzlmZTBlNmVhYzAwMjM2ZmMzMTAwZjVjMzhhODAyY2JlYjk1MWQ3ODcxNTkyYjE2ZDRjN2E5NTlhZTExNWI1ZjYwY2E1NDgxMTdiZWNhZGVkYzAwMmJjYjg5MzI5ZGFkYWY3YjZlMDFiNzdiODNmNWQ2NDZmNTg1ZGJkNTc0Nzk5MjU4NWM1ZjMwMDU1MzRiNWUxNjM5ZTc3Mzk1ZGJhYjEzZTI3MzQ2NDcxODQ5M2M3MDViYzlmM2ZlZmFiNjlhYzIwMDg5MGQ5MTcxZTRiNzQzYTg3YmE3NmI5Y2VjNTY2MGQxOWQ3ZWM0M2I1Yjg4ZTM4OWZiMGUzNDEzMGIxOGVkOGFlYmIxMGVjMWQ2MWE4NTg1YWJmMzE4ZmJkNWUxMGUyZTJjNmZjMDk4YzU3NGYyZWNmNjhiIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiI1YWUwNDhmZWE1ZDI1N2ZkNDE3YzEyMmIzYmRlNDJkZCIsImNvbnRlbnQiOiIwZmQ0Y2E0NmQxNDk3NmYwMzM3MDc3ZGU1MjQxODY5MWEzZTFlOGNjNDgxNjNhMmRmYWNmMWZiOTZkZWYyNDNjNjMwNTY4MDhiNThhOGU4YWZhOWY2ZGY3In0sImp0aSI6ImU5ZjZmZDI3LTA2ZmYtNDg1Yy04ZGQzLWM2ZDZjOWUyODBlZiIsImlhdCI6MTY5OTc2NzY4OCwiZXhwIjoxNjk5NzcxMjg4fQ.o-bTHznRxi2NZrLIjHDgSnY3wxapK_NDWDqhk1skF3g'
->>>>>>> Stashed changes
     }
     
 
@@ -48,15 +44,9 @@ def findRouteAPI(start_coordinates, dest_coordinates, route_type):
     # and overall efficiency
     return route
 
-<<<<<<< Updated upstream
 def calcBestRoute(start_coordinates, dest_coordinates):
     fastest_route = findRouteAPI(start_coordinates, dest_coordinates, 0)
     shortest_route = findRouteAPI(start_coordinates, dest_coordinates, 1)
-=======
-def calcBestRoute(fastest_route, shortest_route):
-    #for route in fastest_route[]:
-    return 1
->>>>>>> Stashed changes
 
     fR = js.loads(fastest_route)
     sR = js.loads(shortest_route)
@@ -77,8 +67,6 @@ def calcBestRoute(fastest_route, shortest_route):
         sRTime.append(route['uncongestedTravelTimeMinutes'])
         sRSpeed.append(route['averageSpeed'])
         sRDistance.append(route['totalDistance'])
-    
-    if():
 
 if __name__ == "__main__":
     start_coordinates = "37.757386%2C-122.490667"

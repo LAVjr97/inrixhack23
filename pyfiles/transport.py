dcc7d7c60c42f9fdd432159faf45443d4a7b403a
LAVjr97 <lavjr97@gmail.com>
2023-11-11 18:40:24 -0800

    added findRouteAPI

--- pyfiles/transport.py
+++ pyfiles/transport.py
@@ -8,6 +8,7 @@ def calcFastestRoute(start_coordinates, dest_coordinates):
     headers = {
         'Authorization': f'Bearer {apikey.TOKEN}'
     }
+    
 
     response = requests.request("GET", url, headers=headers, data=payload)
 
@@ -15,10 +16,47 @@ def calcFastestRoute(start_coordinates, dest_coordinates):
     # STUDENT WORK: customize route so that it gives the fastest route from start to dest 
     # considering total distance and average speed with consideration to safety fast travel
     # and overall efficiency
+
+
+
+
+
     return route
+
+
+def findRouteAPI(start_coordinates, dest_coordinates, route_type):
+    url = f"https://api.iq.inrix.com/findRoute?wp_1={start_coordinates}&wp_2={dest_coordinates}&maxAlternates=2&routeType={route_type}&format=json"
+
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {apikey.TOKEN}'
+    }
+    
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    route = response.text 
+    # STUDENT WORK: customize route so that it gives the fastest route from start to dest 
+    # considering total distance and average speed with consideration to safety fast travel
+    # and overall efficiency
+    return route
+
+def calcBestRoute(fastest_route, shortest_route):
+    for route in fastest_route[]
+
 
 if __name__ == "__main__":
     start_coordinates = "37.757386%2C-122.490667"
     dest_coordinates = "37.746138%2C-122.395481"
+    
+    fastest_route = findRouteAPI(start_coordinates, dest_coordinates, 0)
+    shortest_route = findRouteAPI(start_coordinates, dest_coordinates, 1)
+    opt_route = calcBestRoute(fastest_route, shortest_route)
+
+
+
     route = calcFastestRoute(start_coordinates, dest_coordinates)
     print("route: ", route)
+
+
+

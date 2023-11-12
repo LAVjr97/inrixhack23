f4ed16f7bb1aceec54c4d0b0d1d02bf0ea417142
Divakar Saini <dsaini64@gmail.com>
2023-11-12 07:10:27 -0800

    hopefully this is right

--- client/google-maps-app/src/App.tsx
+++ client/google-maps-app/src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-var coordinates = new Array();
-
 const containerStyle = {
   width: '80%',
   height: '500px',
@@ -18,55 +16,57 @@ function App() {
   const [startPosition, setStartPosition] = useState(center);
   const [clickedPositions, setClickedPositions] = useState<google.maps.LatLngLiteral[]>([]);
   const [coordinatesArray, setCoordinatesArray] = useState<{ lat: number; lng: number }[]>([]);
-
-
+  const [textFieldValues, setTextFieldValues] = useState<string[]>(['', '']);
+  
   const handleMapClick = (event: google.maps.MapMouseEvent | google.maps.IconMouseEvent) => {
     if (clickedPositions.length < 2 && event.latLng) {
       const latLng = event.latLng as google.maps.LatLng;
-      setClickedPositions(prevPositions => [...prevPositions, { lat: latLng.lat(), lng: latLng.lng() }]);
+      const clickedPosition = { lat: latLng.lat(), lng: latLng.lng() };
+
+      // Update coordinatesArray with the first two clicked positions
+      setCoordinatesArray(prevCoordinates => [
+        ...(prevCoordinates.length < 2 ? prevCoordinates : prevCoordinates.slice(2)),
+        clickedPosition,
+      ]);
+
+      setClickedPositions(prevPositions => [...prevPositions, clickedPosition]);
+
+      if (clickedPositions.length === 0) {
+        // Set the start position only once when the first point is clicked
+        setStartPosition(clickedPosition);
+        setTextFieldValues([`${clickedPosition.lat}`, `${clickedPosition.lng}`]);
+      } else {
+        setTextFieldValues(prevValues => [...prevValues, `${clickedPosition.lat}`, `${clickedPosition.lng}`]);
+      }
     }
   };
-  
-  
-  
-  const fetchGeoLocation = async (address: string): Promise<{ lat: number; lng: number }> => {
-    const response = await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ''}`
-    );
-    
 
-    const data = await response.json();
-
-    if (data.results && data.results.length > 0) {
-      const location = data.results[0].geometry.location;
-      return { lat: location.lat, lng: location.lng };
-    } else {
-      throw new Error('Location not found');
-    }
+  const handleSubmit = () => {
+    // Process or use the coordinates as needed
+    console.log('Submitted Coordinates:', coordinatesArray);
+    // You can add further processing or submit the coordinates to a server here
   };
 
   return (
     <div>
-
       <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ''}>
         <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} onClick={handleMapClick}>
           {clickedPositions.map((position, index) => (
-            <Marker key={index} position={position} label={`Point ${index + 1}`} />
+            <Marker key={index} position={position} label={`Point ${index + 0.5}`} />
           ))}
           {startPosition && <Marker position={startPosition} label="Start" />}
         </GoogleMap>
       </LoadScript>
-      {clickedPositions.length > 0 && (
-        <div>
-          <h3>Clicked Positions:</h3>
-          {clickedPositions.map((position, index) => (
-            <div key={index}>
-              <p>Point {index + 1}:</p>
-              <p>Latitude: {position.lat}</p>
-              <p>Longitude: {position.lng}</p>
-            </div>
-          ))}
+
+      {textFieldValues.map((value, index) => (
+        <div key={index}>
+          <label></label>
+          <input type="text" value={value} readOnly />
         </div>
+      ))}
+
+      {clickedPositions.length === 2 && (
+        <button onClick={handleSubmit}>Submit</button>
       )}
     </div>
   );

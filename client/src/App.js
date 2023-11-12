79aaa6ca696b06f45b1f741b366b652770f7b383
tmishra17 <jetmi@live.com>
2023-11-12 04:07:31 -0800

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,49 @@
 import React, { useState, useEffect} from 'react'
+import {Button} from 'antd'
 
-  function App() {
+  const App = () => {
+    const [clicked, setClicked] = useState(false)
     return (
       <div>
-        hello world!!
+  
+          {(clicked) ?(
+            <div>
+                <Results />
+                <Button type='primary' onClick={() => {setClicked(false)}}>Go to Home</Button>
+              </div>
+            ) : (
+              <div>
+                <div>Enter data</div>
+                <Button onClick={()=>{
+                  setClicked(true)
+                }} type='primary'>Submit</Button>
+              </div>
+            )
+          }
+      </div>
+    );
+  }
+  const Results = () => {
+    const [data, setData] = useState([{}])
+    useEffect(() => {
+        fetch("/results")
+          .then(res=>res.json())
+          .then(data => {
+          setData(data)
+          console.log(data)
+        })
+    }, [])
+    return (
+      <div>
+        {(typeof data.results === "undefined")? (
+            <p>loading</p>
+          ): (
+            data.results.map((result, i)=>(
+              <p key={i}>{result}</p>
+            ))
+          )
+        }
       </div>
     )
   }
-
   export default App

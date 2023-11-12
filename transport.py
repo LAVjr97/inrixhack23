b4ba73abecac9c0cea6ea6331ffec6ca7f90c7c1
tmishra17 <jetmi@live.com>
2023-11-11 16:28:12 -0800

    Made CalcFastestRoute function

--- transport.py
+++ transport.py
@@ -1,12 +1,18 @@
 import requests
+import apikey
 
-url = "https://api.iq.inrix.com/findRoute?wp_1=37.770581%2C-122.442550&wp_2=37.765297%2C-122.442527&format=json"
+def calcFastestRoute(start_coordinates, end_coordinates):
+    url = f"https://api.iq.inrix.com/findRoute?wp_1={start_coordinates}&wp_2={end_coordinates}&format=json"
 
-payload = {}
-headers = {
-  'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6Imxlamd5NzNhMTQiLCJ0b2tlbiI6eyJpdiI6ImFkMDQxYzVlYTY5YzI2OTgyMGQ2MjYzYmU5NGEzODBlIiwiY29udGVudCI6ImQ1MzA3MzE0MzIyNjIzYjFhN2RlNGQ0NTZlNmQwOGY5YTM5NDQwMGY4NTQ5NTkwMTY3ODEwZTI1YzlhNWI2MDc4NGE0ZTVmOTlmMjZmODFiZjMwYzY3YjdlNGJiNzU2N2U4MTZjODk2MTZjNTVmYTVhMWQ2MjM0ZjY5OGRhNWE4YTdlMGJkNjMwOTk3MWViMjlkZWFjNzMxMzliNzAyYmQ4MTZhMTE0YmM1ZDk2NzU0NzZkZGFjZjNkYWUxNmIwYzQ0YTc3MzUwNDEyYmY0ZWQ4YmNkZDI2YzY0NmFkMGVjYjdhZTY1ZGQ5MTVhYzhlN2RhOTYyOTI4Y2Y2Nzg1NDlmNWU0NmFlZDM3ZjQwYTA3ZGZlYmNjYzAyNDJmZTUzYTEzNDJlYmU4YWQ4OTJkZjZkMjRmNzU2MmQ3ODhiZmYwZjVmMTY3NTI4NTRmYzcwMmY4MTZmMGY4YjM0MWVkZTkwNTc3ZGEzMDlhYzY0OTIyY2ViOWZlYzQzZGVhYTFhMTQ0OGE0NTQ5Mjg5YmE1YzZhY2Y3ZDlhMDAxNzVjODg4ZTFkZTc4MmY4Y2U5NjgzY2U5MGUzZGMxNGIwNDIyY2ZjNzg3YTZmMWZhMDQ0YTVmNTYxOGY2MWQ0NzBiNDQ3MzIxNjIwNjg3YmVmNjMzYmEzMzFjNDM2MTdhZmMzNmNiOTA4MTk2MmNmNjZlNDY0M2NjMjQ2N2UxNTdmYjc1MjBiZmI0MTBlNWEwMmFlNTAyMzA1MDlkYzMyYmVmNzA0ZjhmYjgxNzQzYTg2ODcyYjk4M2I4YjE1MGY3MmYxMzlmNzU2NDUzYmY5OGQ3OTY2Y2E1OTI5NGI5MDM0YjU2NTcyMmJkOGVjZTA2NGFjY2ZhNGU1NDA0In0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiJhZDA0MWM1ZWE2OWMyNjk4MjBkNjI2M2JlOTRhMzgwZSIsImNvbnRlbnQiOiJkODJlN2IzOTA2NTA3M2YyZDlkOTY5NGE3YTc2Mzk5YzlhYWM2MDMxYTk2MDcxNDI0ZTg3MDg2NmNjODhmNTJhZTZkNWMxODdlYjExZDUzOGM4MGI2Njg5In0sImp0aSI6IjgxOWY0ZDExLTQxMzAtNGU0Zi05NTU4LWI2NjdiNmFmMTcxYyIsImlhdCI6MTY5OTc0NDA0NSwiZXhwIjoxNjk5NzQ3NjQ1fQ.H5O9ySaAt0iUj63McoZ2KIFh4b-Dm2ijyVv0UISeqiE'
-}
+    payload = {}
+    headers = {
+    'Authorization': f'Bearer {apikey.TOKEN}'
+    }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
+    print(response.text)
+    return 1
+
+if __name__ == "__main__":
+    calcFastestRoute("37.757386%2C-122.490667","37.765297%2C-122.442527")
